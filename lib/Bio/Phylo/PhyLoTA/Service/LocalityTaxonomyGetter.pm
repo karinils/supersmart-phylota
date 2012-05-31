048ac5d5c16137a543cccdbda5ca227dfe0d9a2b
Rutger Vos <rutgeraldo@gmail.com>
2012-05-31 10:55:47 +0200

    Adding placeholder method stubs

--- lib/Bio/Phylo/PhyLoTA/Service/LocalityTaxonomyGetter.pm
+++ lib/Bio/Phylo/PhyLoTA/Service/LocalityTaxonomyGetter.pm
@@ -8,5 +8,30 @@ sub new {
     return $self;
 }
 
+sub get_localities_for_taxon {
+    my ( $self, $taxon ) = @_;
+    if ( my $gbif = $taxon->gbif ) {
+        
+        # do occurrence lookup
+        # parse gbif result
+        # return LocalityTaxonomy objects
+    }
+    else {
+        
+    }
+}
+
+sub get_gbif_for_taxon {
+    my ( $self, $taxon ) = @_;
+    if ( my $gbif = $taxon->gbif ) {
+        return $gbif;
+    }
+    else {
+        # do name lookup
+        # parse gbif result
+        # return gbif id
+    }
+}
+
 
 1;
