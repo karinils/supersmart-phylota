80a37d0b71c60377bdb0e50eaebd9ff6d546bc39
Rutger Vos <rutgeraldo@gmail.com>
2012-06-01 19:20:38 +0200

    no uses the config object and is now valid code under 'use strict';

--- script/blast/blast_allall.pl
+++ script/blast/blast_allall.pl
@@ -12,64 +12,63 @@
 # Careful of what happens to tfile and qfile...
 # Generic SGE script to do all against all BLAST using a Phylota Browser style config file but user supplied inputs on
 # the qsub command line...(see below for syntax of command line args); can also use it to blast one file against another...
-use lib '/home/sanderm/blast'
-  ; # have to tell PERL (running on slave nodes) where to find the module (this is on the head node)
-use pb;
+use strict;
+use Getopt::Long;
+use Bio::Phylo::PhyLoTA::Config;
+
 ##############################
-$blastOutLimit = 250
-  ; # number of hits reported in any one blast search!!!! Note impact of this on all-all searches
+# number of hits reported in any one blast search!!!! Note impact of this on all-all searches
+my $blastOutLimit = 250; 
 ##############################
-$ARGC = @ARGV;
-if ( $ARGC == 0 ) { printUsage(); }
-while ( $fl = shift @ARGV ) {
-    $option = shift @ARGV;
-    if ( $fl =~ /-q/ ) { $qfileName   = $option; }
-    if ( $fl =~ /-t/ ) { $tfileName   = $option; }
-    if ( $fl =~ /-o/ ) { $outfileName = $option; }
-    if ( $fl =~ /-c/ ) { $configFile  = $option; }
-}
-%pbH     = %{ pb::parseConfig($configFile) };
-$release = pb::currentGBRelease();
-if (   $outfileName eq ""
-    || $qfileName  eq ""
-    || $tfileName  eq ""
-    || $configFile eq "" )
-{
+
+# process command line arguments
+my ( $qfile, $tfile, $outfileStem, $configfile );
+GetOptions(
+    'qfile=s'      => \$qfile,
+    'tfile=s'      => \$tfile,
+    'outfile=s'    => \$outfileStem,
+    'configfile=s' => \$configfile,
+);
+my $config = Bio::Phylo::PhyLoTA::Config->new($configfile);
+my $release = $config->currentGBRelease();
+unless ( $outfileStem && $qfile && $tfile && $configfile ) {
     print "Missing a required command line option\n";
     printUsage();
 }
-if ( !( -e $qfileName && -e $tfileName ) ) {
+if ( !( -e $qfile && -e $tfile ) ) {
     die("allallblast: input file(s) are missing\n");
 }
-$headWorkingDir  = $pbH{'HEAD_WORKING_DIR'};
-$slaveWorkingDir = $pbH{'SLAVE_WORKING_DIR'};
+
+
+my $headWorkingDir  = $config->HEAD_WORKING_DIR;
+my $slaveWorkingDir = $config->SLAVE_WORKING_DIR;
 my $taskId = $ENV{JOB_ID};    # used to provide unique file names
-my $outfile    = "$slaveWorkingDir/$outfileName.$taskId";
+my $outfile    = "$slaveWorkingDir/$outfileStem.$taskId";
 my $dbfile     = "$slaveWorkingDir/blastdb.$taskId";
 my $fmtlogfile = "$slaveWorkingDir/fmtlog.$taskId";
 
 # Run formatdb prior to blast...
-$blastDir = $pbH{'BLAST_DIR'};
-$formatdbCom =
+my $blastDir = $config->BLAST_DIR;
+my $formatdbCom =
     "$blastDir/bin/formatdb"
-  . " -i $tfileName"
+  . " -i $tfile"
   . " -l $fmtlogfile"
-  . " -p $pbH{'PROTEIN_FLAG'}"
-  . " -o $pbH{'PARSE_SEQID_FLAG'}"
+  . " -p " . $config->PROTEIN_FLAG
+  . " -o " . $config->PARSE_SEQID_FLAG
   . " -n $dbfile"; # write the database files to the slave node with this prefix
 print "formatting database with: $formatdbCom ($formatdbCom)\n";
 system($formatdbCom);
-$blastCom =
+my $blastCom =
     "$blastDir/bin/blastall"
   . " -b $blastOutLimit"
-  . " -i $qfileName"
+  . " -i $qfile"
   . " -o $outfile"
-  . " -e $pbH{'BLAST_EXPECT'}"
-  . " -F $pbH{'BLAST_DUST'}"
-  . " -p $pbH{'BLAST_PROGRAM'}"
-  . " -S $pbH{'BLAST_STRAND'}"
+  . " -e " . $config->BLAST_EXPECT
+  . " -F " . $config->BLAST_DUST
+  . " -p " . $config->BLAST_PROGRAM
+  . " -S " . $config->BLAST_STRAND
   . " -d $dbfile"
-  . " -m $pbH{'BLAST_OUTPUT_FMT'}";
+  . " -m " . $config->BLAST_OUTPUT_FMT;
 print "...$blastCom\n";
 print "...running Blast NxN ($blastCom)\n";
 system($blastCom);
