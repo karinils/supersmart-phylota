06285d67045feaa0943f5ce38dedf58688821837
Rutger Vos <rutgeraldo@gmail.com>
2012-06-01 19:25:08 +0200

    no longer uses pb module, uses Getopt::Long for command line arguments and the config object to get the currentGBRelease

--- script/blast/pb_query_release.pl
+++ script/blast/pb_query_release.pl
@@ -1,10 +1,17 @@
-#!/usr/bin/perl -w
-use pb;
-while ( $fl = shift @ARGV ) {
-    $par = shift @ARGV;
-    if ( $fl =~ /-c/ ) { $configFile = $par; }
-}
-%pbH     = %{ pb::parseConfig($configFile) };
-$release = pb::currentGBRelease();
-print
-"The current release setting based on the config file $configFile is $release\n";
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Getopt::Long;
+use Bio::Phylo::PhyLoTA::Config;
+
+# process command line arguments
+my $configFile;
+GetOptions( 'configFile=s' => \$configFile );
+
+# instantiate config object
+my $config = Bio::Phylo::PhyLoTA::Config->new($configFile);
+
+# print output
+print "The current release setting based on the config file $configFile is ",
+    $config->currentGBRelease, "\n";
+
