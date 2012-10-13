3a39d21e347c05e9301df6a8cdb2ed0418de0ebe
Rutger Vos <rutgeraldo@gmail.com>
2012-10-13 19:19:34 +0200

    inparanoid table now uses MyISAM

--- script/inparanoid.sql
+++ script/inparanoid.sql
@@ -1,8 +1,9 @@
-create table if not exists inparanoid(
+create table if not exists inpara(
 	id int(10) unsigned not null,
 	guid int(10) unsigned not null,
 	filename varchar(64) default null,
-	confidence float default NULL,
+	confidence float default null,
 	protid varchar(64) default null,
-	bootstrap int(10)
-) ENGINE=INNODB;
+	key(protid),
+	bootstrap varchar(4)
+) ENGINE=MyISAM;
