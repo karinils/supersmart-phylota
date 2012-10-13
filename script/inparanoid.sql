0027bf6f4b5d48c7453d8a582167442da66888f1
Rutger Vos <rutgeraldo@gmail.com>
2012-10-13 17:45:40 +0200

    simplified inparanoid table schema: no indices, so data should load faster

--- script/inparanoid.sql
+++ script/inparanoid.sql
@@ -1,11 +1,8 @@
 create table if not exists inparanoid(
 	id int(10) unsigned not null,
-	index(id),
 	guid int(10) unsigned not null,
-	index(guid),
 	filename varchar(64) default null,
 	confidence float default NULL,
 	protid varchar(64) default null,
-	index(protid),
 	bootstrap int(10)
 ) ENGINE=INNODB;
