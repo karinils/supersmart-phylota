22246ab9bf6eab3ebf33bb27156e0343659654e8
Rutger Vos <rutgeraldo@gmail.com>
2012-10-16 22:47:23 +0200

    added hack to translate between search clauses that operate on ci_gi and on cluster. this hack is necessary because of the strange way the cluster and ci_gi tables are designed. it would have been so much better if there was a simple primary/foreign key relationship, but instead there is a compound key of cluster.ti_root,cluster.ci,cluster.cl_type that matches cigi.ti,cigi.clustid,cigi.cl_type

--- lib/Bio/Phylo/PhyLoTA/Service.pm
+++ lib/Bio/Phylo/PhyLoTA/Service.pm
@@ -120,6 +120,23 @@ sub single_node {
 
 sub single_cluster {
 	my ( $self, $clause ) = @_;
+	
+	# this hack is necessary because of the strange way the cluster and ci_gi
+	# tables are designed. it would have been so much better if there was a
+	# simple primary/foreign key relationship, but instead there is a compound
+	# key of cluster.ti_root,cluster.ci,cluster.cl_type that matches
+	# cigi.ti,cigi.clustid,cigi.cl_type
+	if ( my $value = $clause->{'clustid'} ) {
+		$clause->{'ci'} = $value;
+		delete $clause->{'clustid'};
+		$logger->info("search clause included 'clustid', changed this to 'ci'");
+	}
+	if ( my $value = $clause->{'ti'} ) {
+		$clause->{'ti_root'} = $value;
+		delete $clause->{'ti'};
+		$logger->info("search clause included 'ti', changed this to 'ti_root'");
+	}	
+	
 	my $result;
 	eval {
 		$result = $schema->resultset('Cluster')->single($clause);
