6fb21ba2348f1702f3f8350deeddb2455fd98749
Rutger Vos <rutgeraldo@gmail.com>
2012-11-12 10:43:05 +0100

    added some implementation for calibration table

--- lib/Bio/Phylo/PhyLoTA/Domain/CalibrationTable.pm
+++ lib/Bio/Phylo/PhyLoTA/Domain/CalibrationTable.pm
@@ -6,8 +6,22 @@ use warnings;
 
 sub new {
     my $class = shift;
-    my $self = bless {}, $class;
+    my $self = bless [], $class;
     return $self;
+}
+
+sub add_row {
+    my ( $self, %args ) = @_;
+    push @{ $self }, \%args;
+}
+
+sub to_string {
+    my $self = shift;
+    my $string = '';
+    for my $row ( @{ $self } ) {
+        $string .= join(' ', @{ $row->{taxa} }, '|', $row->{min_age}, $row->{max_age} ) . "\n";
+    }
+    return $string;
 }
 
 
@@ -19,7 +33,8 @@ Bio::Phylo::PhyLoTA::Domain::CalibrationTable - Calibration table
 
 =head1 DESCRIPTION
 
-Table containing: order, family, genus. 
+Object that represents a list of calibration points that can be serialized
+as input for PhyTime.
 
 =cut
 
