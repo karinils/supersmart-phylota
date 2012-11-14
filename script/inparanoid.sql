226c8961289223c0b908a9ba988ba6ed986eeb13
Karin Nilsson <karin.sara.nilsson@gmail.com>
2012-11-14 11:33:21 +0100

    removing old inparanoid sql script

--- script/inparanoid.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists inparanoid(
-	id int(10) unsigned not null,
-	guid int(10) unsigned not null,
-	filename varchar(64) default null,
-	confidence float default null,
-	protid varchar(64) default null,
-	key(protid),
-	bootstrap varchar(4)
-) ENGINE=MyISAM;
